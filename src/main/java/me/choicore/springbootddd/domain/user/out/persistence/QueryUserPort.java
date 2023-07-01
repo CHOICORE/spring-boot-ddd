0d6eeecc3fd4a539c60b3f34d9da935126898501
CHOICORE <choijaehyeong93@gmail.com>
2023-07-01 21:43:45 +0900

    feat: rename classes

--- src/main/java/me/choicore/springbootddd/domain/user/out/persistence/QueryUserPort.java
+++ src/main/java/me/choicore/springbootddd/domain/user/out/persistence/QueryUserPort.java
@@ -2,18 +2,17 @@ package me.choicore.springbootddd.domain.user.out.persistence;
 
 import me.choicore.springbootddd.domain.user.model.QueryUserProfile;
 import me.choicore.springbootddd.domain.user.model.UserProfile;
-import me.choicore.springbootddd.domain.user.model.Username;
 
 import java.util.List;
 
 public interface QueryUserPort {
 
-    UserProfile findById(final Long userId);
+    UserProfile findById(Long userId);
 
-    List<UserProfile> findByUserProfile(final QueryUserProfile user);
+    List<UserProfile> findBy(QueryUserProfile user);
 
     List<UserProfile> findAll();
 
-    boolean existsByUsername(final Username username);
+    boolean existsByUsername(String username);
 
 }

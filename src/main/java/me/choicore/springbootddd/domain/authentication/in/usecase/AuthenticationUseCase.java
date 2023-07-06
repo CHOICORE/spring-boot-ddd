e969adf8de84682d417e198fbaae489da6284470
CHOICORE <choijaehyeong93@gmail.com>
2023-07-06 10:11:35 +0900

    refactor: changed package structure

--- src/main/java/me/choicore/springbootddd/domain/authentication/in/usecase/AuthenticationUseCase.java
+++ src/main/java/me/choicore/springbootddd/domain/authentication/in/usecase/AuthenticationUseCase.java
@@ -4,4 +4,6 @@ public interface AuthenticationUseCase {
 
     void authenticateBy(final String identifier, final String password);
 
+    Credentials authenticateBy(String identifier, String password);
+
 }
